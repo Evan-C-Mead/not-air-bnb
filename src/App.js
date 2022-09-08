8516cf1f337efb95f29eb5d6d2568be3bf3e9227
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-08 01:48:25 -0500

    card section complete - refactoring for props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import './css/App.css';
+import Card from "./components/Card";
 
 function App() {
   return (
       <div>
         <Navbar />
         <Hero />
+        <Card />
       </div>
   )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import cardImage from '../images/katie-zaferes.png'
+import star from '../images/star.png'
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img src={cardImage} alt="cardIMG" className="card--image" />
+            <div className="card--stats">
+                <img src={star} alt="star" className="card--star"/>
+                <span>5.0</span>
+                <span className="grayText">(6) • </span>
+                <span className="grayText">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="boldText">From $136 / person</span></p>
+        </div>
+    )
+}
