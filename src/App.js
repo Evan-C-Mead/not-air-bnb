8f96fbb666047610f67c26156d48fbc14bcec375
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-08 01:48:25 -0500

    learned props - destructuring

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import './css/App.css';
+import Card from "./components/Card";
+import Contact from "./components/Contact";
+import mrW from '../src/images/mr-whiskerson.png'
+import fluffykins from '../src/images/fluffykins.png'
+import felix from '../src/images/felix.png'
+import pumpkin from '../src/images/pumpkin.png'
 
 function App() {
-  return (
-      <div>
-        <Navbar />
-        <Hero />
-      </div>
-  )
+    return (
+        // <div>
+        //     <Navbar/>
+        //     <Hero/>
+        //     <Card/>
+        // </div>
+
+        <div className="contacts">
+            <Contact
+                img={mrW}
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact
+                img={fluffykins}
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact
+                img={felix}
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact
+                img={pumpkin}
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,73 @@
+import React from "react"
+import phone from '../images/phone-icon.png';
+import mail from '../images/mail-icon.png';
+
+export default function Contact(props) {
+    console.log(props);
+    return (
+        <div className="contact-card">
+            <img src={props.img} alt="mr-whiskerton"/>
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src={phone} alt="phone"/>
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src={mail} alt="mail"/>
+                <p>{props.email}</p>
+            </div>
+        </div>
+    )
+}
+
+// <div className="contact-card">
+//     <img src="./images/mr-whiskerson.png"/>
+//     <h3>Mr. Whiskerson</h3>
+//     <div className="info-group">
+//         <img src="./images/phone-icon.png" />
+//         <p>(212) 555-1234</p>
+//     </div>
+//     <div className="info-group">
+//         <img src="./images/mail-icon.png" />
+//         <p>mr.whiskaz@catnap.meow</p>
+//     </div>
+// </div>
+
+// <div className="contact-card">
+//     <img src="./images/fluffykins.png"/>
+//     <h3>Fluffykins</h3>
+//     <div className="info-group">
+//         <img src="./images/phone-icon.png" />
+//         <p>(212) 555-2345</p>
+//     </div>
+//     <div className="info-group">
+//         <img src="./images/mail-icon.png" />
+//         <p>fluff@me.com</p>
+//     </div>
+// </div>
+
+// <div className="contact-card">
+//     <img src="./images/felix.png"/>
+//     <h3>Felix</h3>
+//     <div className="info-group">
+//         <img src="./images/phone-icon.png" />
+//         <p>(212) 555-4567</p>
+//     </div>
+//     <div className="info-group">
+//         <img src="./images/mail-icon.png" />
+//         <p>thecat@hotmail.com</p>
+//     </div>
+// </div>
+
+// <div className="contact-card">
+//     <img src="./images/pumpkin.png"/>
+//     <h3>Pumpkin</h3>
+//     <div className="info-group">
+//         <img src="./images/phone-icon.png" />
+//         <p>(0800) CAT KING</p>
+//     </div>
+//     <div className="info-group">
+//         <img src="./images/mail-icon.png" />
+//         <p>pumpkin@scrimba.com</p>
+//     </div>
+// </div>
