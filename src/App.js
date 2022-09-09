e739c2dbb66ff975421150dd7b553a269b7be236
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-09 18:21:50 -0500

    messing around

--- src/App.js
+++ src/App.js
@@ -11,37 +11,37 @@ import pumpkin from '../src/images/pumpkin.png';
 
 function App() {
     return (
-        // <div>
-        //     <Navbar/>
-        //     <Hero/>
-        //     <Card/>
-        // </div>
+        <div>
+            <Navbar/>
+            <Hero/>
+            <Card/>
 
-        <div className="contacts">
-            <Contact
-                img={mrW}
-                name="Mr. Whiskerson"
-                phone="(212) 555-1234"
-                email="mr.whiskaz@catnap.meow"
-            />
-            <Contact
-                img={fluffykins}
-                name="Fluffykins"
-                phone="(212) 555-2345"
-                email="fluff@me.com"
-            />
-            <Contact
-                img={felix}
-                name="Felix"
-                phone="(212) 555-4567"
-                email="thecat@hotmail.com"
-            />
-            <Contact
-                img={pumpkin}
-                name="Pumpkin"
-                phone="(0800) CAT KING"
-                email="pumpkin@scrimba.com"
-            />
+            <div className="contacts">
+                <Contact
+                    img={mrW}
+                    name="Mr. Whiskerson"
+                    phone="(212) 555-1234"
+                    email="mr.whiskaz@catnap.meow"
+                />
+                <Contact
+                    img={fluffykins}
+                    name="Fluffykins"
+                    phone="(212) 555-2345"
+                    email="fluff@me.com"
+                />
+                <Contact
+                    img={felix}
+                    name="Felix"
+                    phone="(212) 555-4567"
+                    email="thecat@hotmail.com"
+                />
+                <Contact
+                    img={pumpkin}
+                    name="Pumpkin"
+                    phone="(0800) CAT KING"
+                    email="pumpkin@scrimba.com"
+                />
+            </div>
         </div>
     )
 }
