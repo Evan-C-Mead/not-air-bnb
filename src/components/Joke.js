4f9e481ee8650bf825269ed221fdcf9709b74edc
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-11 23:44:08 -0500

    added pun bool

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,8 +3,9 @@ import React from "react"
 export default function Joke(props) {
     return (
         <div>
-            <h3>{props.setup}</h3>
-            <h3>{props.punchline}</h3>
+            <h3 style={{display: props.setup ? "block" : "none"}}>{props.setup}</h3>
+            <p>{props.punchline}</p>
+            <p>Is this a pun? - {props.isPun.toString()}</p>
             <hr />
         </div>
     )
