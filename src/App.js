bf86154b45a64f6c9afc54af7c6f92efe0b624e7
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-13 00:26:44 -0500

    small styling changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import './css/App.css';
 import Card from "./components/Card";
-import Contact from "./components/Contact";
-import mrW from '../src/images/mr-whiskerson.png';
-import fluffykins from '../src/images/fluffykins.png';
-import felix from '../src/images/felix.png';
-import pumpkin from '../src/images/pumpkin.png';
-import Joke from './components/Joke';
 
 export default function App() {
     return (
