93a4e6be0df795672d4361e2ba01f53f0f5640cf
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-16 09:06:10 -0500

    refactored

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 export default function Joke(props) {
     return (
@@ -6,7 +6,7 @@ export default function Joke(props) {
             <h3 style={{display: props.setup ? "block" : "none"}}>{props.setup}</h3>
             <p>{props.punchline}</p>
             <p>Is this a pun? - {props.isPun.toString()}</p>
-            <hr />
+            <hr/>
         </div>
     )
 }
