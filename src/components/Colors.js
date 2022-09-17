2ac48d26c6663f2166c46fccb8f757d2f7d1ece2
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-17 08:07:20 -0500

    styled Colors.js

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -2,17 +2,31 @@ import React from "react";
 
 export default function Colors() {
     const colors = [
-        <h3>Red</h3>,
-        <h3>Orange</h3>,
-        <h3>Yellow</h3>,
-        <h3>Green</h3>,
-        <h3>Blue</h3>,
-        <h3>Indigo</h3>,
-        <h3>Violet</h3>
+        <span className="redText">
+            <h3>Red</h3>
+        </span>,
+        <span className="orangeText">
+            <h3>Orange</h3>
+        </span>,
+        <span className="yellowText">
+            <h3>Yellow</h3>
+        </span>,
+        <span className="greenText">
+            <h3>Green</h3>
+        </span>,
+        <span className="blueText">
+            <h3>Blue</h3>
+        </span>,
+        <span className="indigoText">
+            <h3>Indigo</h3>
+        </span>,
+        <span className="violetText">
+            <h3>Violet</h3>
+        </span>
     ]
 
     return (
-        <div>
+        <div className="colorsList">
             {colors}
         </div>
     )
