11e89e24559b7c0c7e4ac931a7c98e1a176e08c5
Evan-C-Mead <evan.c.mead@gmail.com>
2022-09-18 18:21:19 -0500

    styled Colors.js - changed class names to more appropriate name

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -2,25 +2,25 @@ import React from "react";
 
 export default function Colors() {
     const colors = [
-        <span className="redText">
+        <span className="redHeader">
             <h3>Red</h3>
         </span>,
-        <span className="orangeText">
+        <span className="orangeHeader">
             <h3>Orange</h3>
         </span>,
-        <span className="yellowText">
+        <span className="yellowHeader">
             <h3>Yellow</h3>
         </span>,
-        <span className="greenText">
+        <span className="greenHeader">
             <h3>Green</h3>
         </span>,
-        <span className="blueText">
+        <span className="blueHeader">
             <h3>Blue</h3>
         </span>,
-        <span className="indigoText">
+        <span className="indigoHeader">
             <h3>Indigo</h3>
         </span>,
-        <span className="violetText">
+        <span className="violetHeader">
             <h3>Violet</h3>
         </span>
     ]
